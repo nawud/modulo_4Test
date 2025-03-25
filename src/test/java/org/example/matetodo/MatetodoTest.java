0083f52f18f3da0d01f43730bbe577fb227b09b7
lltosso <luis.tosso@gmail.com>
2025-03-25 14:43:51 +0100

    added resta

--- src/test/java/org/example/matetodo/MatetodoTest.java
+++ src/test/java/org/example/matetodo/MatetodoTest.java
@@ -3,6 +3,7 @@ package org.example.matetodo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MatetodoTest {
 
@@ -68,5 +69,48 @@ public class MatetodoTest {
 
         // Assert
         assertEquals(0, resultado);
+    }
+
+    // Operacion de una resta
+    @Test
+    public void testRestar_numerosPositivos_retornaResultadoCorrecto() {
+        // Arrange
+        Matetodo matetodo = new Matetodo();
+        float numero1 = 10.5f;
+        float numero2 = 4.3f;
+
+        // Act
+        float resultado = matetodo.restar(numero1, numero2);
+
+        // Assert
+        assertEquals(6.2f, resultado);
+    }
+
+    @Test
+    public void testRestar_numerosNegativos_retornaResultadoCorrecto() {
+        // Arrange
+        Matetodo matetodo = new Matetodo();
+        float numero1 = -8.5f;
+        float numero2 = -3.5f;
+
+        // Act
+        float resultado = matetodo.restar(numero1, numero2);
+
+        // Assert
+        assertEquals(-5.0f, resultado);
+    }
+
+    @Test
+    public void testRestar_numeroPositivoYNegativo_retornaResultadoCorrecto() {
+        // Arrange
+        Matetodo matetodo = new Matetodo();
+        float numero1 = 9;
+        float numero2 = 4;
+
+        // Act
+        float resultado = matetodo.restar(numero1, numero2);
+
+        // Assert
+        assertEquals(5, resultado);
     }
 }

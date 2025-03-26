18d11af334c754287385eaf102524edcc151a73d
lltosso <luis.tosso@gmail.com>
2025-03-26 13:53:05 +0100

    Module 4 completed

--- src/main/java/org/example/matetodo/Matetodo.java
+++ src/main/java/org/example/matetodo/Matetodo.java
@@ -16,25 +16,7 @@ public class Matetodo {
     }
 
 
-    public float restar(float n1, float n2) {
-        return n1 - n2;
-    }
 
-    public int restarParesConsecutivos(int limite) {
-        int resta = 0;
-        boolean primerParEncontrado = false;
-
-        for (int i = 2; i <= limite; i += 2) {
-            if (!primerParEncontrado) {
-                resta = i;
-                primerParEncontrado = true;
-            } else {
-                resta -= i;
-            }
-        }
-
-        return resta;
-    }
 
 
 }
--- src/test/java/org/example/matetodo/RestaTest.java
+++ src/test/java/org/example/matetodo/RestaTest.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RestaTest {
-    // Operacion de una resta
+
     @Test
     public void testRestar_numerosPositivos_retornaResultadoCorrecto() {
         // Arrange
-        Matetodo matetodo = new Matetodo();
+        Resta resta = new Resta();
         float numero1 = 10.5f;
         float numero2 = 4.3f;
 
         // Act
-        float resultado = matetodo.restar(numero1, numero2);
+        float resultado = resta.restar(numero1, numero2);
 
         // Assert
         assertEquals(6.2f, resultado);
@@ -23,12 +23,12 @@ public class RestaTest {
     @Test
     public void testRestar_numerosNegativos_retornaResultadoCorrecto() {
         // Arrange
-        Matetodo matetodo = new Matetodo();
+        Resta resta = new Resta();
         float numero1 = -8.5f;
         float numero2 = -3.5f;
 
         // Act
-        float resultado = matetodo.restar(numero1, numero2);
+        float resultado = resta.restar(numero1, numero2);
 
         // Assert
         assertEquals(-5.0f, resultado);
@@ -37,12 +37,12 @@ public class RestaTest {
     @Test
     public void testRestar_numeroPositivoYNegativo_retornaResultadoCorrecto() {
         // Arrange
-        Matetodo matetodo = new Matetodo();
+        Resta resta = new Resta();
         float numero1 = 9;
         float numero2 = 4;
 
         // Act
-        float resultado = matetodo.restar(numero1, numero2);
+        float resultado = resta.restar(numero1, numero2);
 
         // Assert
         assertEquals(5, resultado);
